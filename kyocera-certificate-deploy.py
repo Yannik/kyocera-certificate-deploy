7d1f2cea601077b9c6fdf8e55bbb007dd2735684
Yannik Sembritzki <yannik@sembritzki.me>
2020-05-28 12:54:52 +0200

    Update for using firefox driver

--- kyocera-certificate-deploy.py
+++ kyocera-certificate-deploy.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import logging
@@ -8,6 +8,8 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.firefox.options import Options
 import sys
 from time import sleep
 
@@ -37,60 +39,78 @@ ch.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
 logging.getLogger().addHandler(ch)
 
 
-driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
+#driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
+
+options = Options()
+options.add_argument("--headless")
+
+profile = webdriver.FirefoxProfile()
+profile.DEFAULT_PREFERENCES['frozen']['security.tls.version.enable-deprecated'] = True
+
+capabilities = DesiredCapabilities.FIREFOX.copy()
+capabilities['acceptInsecureCerts'] = True
+driver = webdriver.Firefox(capabilities=capabilities, firefox_profile=profile, options=options)
 driver.set_window_size(1024, 768)
 
 driver.get(host)
 stepshot(driver, screenshots, 1)
 
-driver.switch_to_frame("main")
+WebDriverWait(driver, 15).until(EC.frame_to_be_available_and_switch_to_it("main"))
 
 # Click "Anmelden" button
+print("Clicking anmelden")
 driver.find_element_by_class_name('w10pt').click()
-# apparantly we don't need to select the frame again here
 stepshot(driver, screenshots, 2)
+
+print("Entering password and submitting form")
 password_field = driver.find_element_by_css_selector('input[name="arg02_Password"]')
 password_field.send_keys(password)
 password_field.send_keys(Keys.RETURN)
+WebDriverWait(driver, 15).until(EC.frame_to_be_available_and_switch_to_it("main"))
 stepshot(driver, screenshots, 3)
 
-# apparantly selecing the frame again is necessary here
-driver.switch_to_frame("main")
-# Click "Allgemein" button
-nav_elements = driver.find_elements_by_css_selector('div#nav td a')
-nav_elements[1].click()
+print('Clicking "Allgemein" nav-button')
+allgemein_button = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//div[@id="nav"]//a[contains(@onclick, "basic.htm")]'))
+)
+allgemein_button.click()
 stepshot(driver, screenshots, 4)
 
-# Hover "Zertifikate" menu
+print('Hovering "Zertifikate" menu')
 certificate_nav_top_menu = driver.find_element_by_xpath('//div[@id="Certificates"]/parent::div')
 action_chains = ActionChains(driver).move_to_element(certificate_nav_top_menu).perform()
 stepshot(driver, screenshots, 5)
 
-# Click "Geraetezertifikat" button
+print('Click "Geraetezertifikat" button')
 certificate_nav_submenu_elements = driver.find_elements_by_css_selector('div#Certificates a')
 certificate_nav_submenu_elements[0].click()
 stepshot(driver, screenshots, 6)
 
-# Click "Delete Certificate" button
 delete_button = driver.find_element_by_xpath('//input[contains(@onclick, "CtfDelConf")]')
 if delete_button.get_attribute('disabled') is None:
+  print('Click "Delete Certificate" button')
   delete_button.click()
   stepshot(driver, screenshots, 8)
   driver.find_element_by_css_selector('input[name="Delete"]').click()
+  # Hier gibt's jetzt einen alert
+  driver.switch_to.alert.accept()
   stepshot(driver, screenshots, 9)
 
-# Click "Importieren" button
+print('Click "Importieren" button')
 import_button = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH, '//input[contains(@onclick, "Import")]'))
 )
 import_button.click()
 stepshot(driver, screenshots, 10)
 
+print('Selecting file and submitting form')
 file_input = driver.find_element_by_css_selector('input[name="arg04_inputFile"]')
 password_input = driver.find_element_by_css_selector('input#arg02_password')
 file_input.send_keys(os.path.abspath(certfile))
 stepshot(driver, screenshots, 11)
 password_input.send_keys(Keys.RETURN)
+# hier gibt's umgehend nach button click einen alert
+driver.switch_to.alert.accept()
 stepshot(driver, screenshots, 12)
 
 WebDriverWait(driver, 15).until(
@@ -98,8 +118,11 @@ WebDriverWait(driver, 15).until(
 )
 stepshot(driver, screenshots, 13)
 
+print('Opening "Zurücksetzen" page')
 driver.find_element_by_xpath('//div[@id="leftcolmn"]//a[contains(@onclick, "Reset.htm")]').click()
 stepshot(driver, screenshots, 14)
+
+print('Clicking "Netzwerk neustarten" button')
 driver.find_element_by_xpath('//input[@name="func" and @value="restartNetworkInterface"]/../input[@type="submit"]').click()
 
 WebDriverWait(driver, 45).until_not(
